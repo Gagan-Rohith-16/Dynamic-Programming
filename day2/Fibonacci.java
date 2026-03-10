478ff748823c6aeed002064b5765bc3cbe4ca4cc
K GAGAN ROHITH <161939027+Gagan-Rohith-16@users.noreply.github.com>
2026-03-10 09:34:01 +0530

    Create Fibonacci.java

---
+++ day2/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Fibonacci {
+ static int fib(int n) {
+        if(n==0)
+        return n;
+        if(n==1)
+        return n;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number to find fibanocci:");
+        int num=sc.nextInt();
+        System.out.println("fibanocci of "+num+" is:"+fib(num));
+        sc.close();
+    }
+}
